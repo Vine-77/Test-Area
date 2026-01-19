5b15cc75c36b64db895496cfd334e5621f865672
Vine 77 <shemadivin7@gmail.com>
2026-01-19 08:50:28 +0200

    Added some finishing touches

--- test.js
+++ test.js
@@ -1,23 +1,5 @@
-import readline from 'readline'; // Use import instead of require
-
-const Goat = new Promise((resolve, reject) => {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.question('Who is the football Goat? ', (answer) => {
-        setTimeout(() => {
-            if (answer.trim() === "Ronaldo" || answer.trim() === "Cristiano") {
-                resolve("You're damn right!!");
-            } else {
-                reject("Wrong answer!");
-            }
-            rl.close();
-        }, 2000);
-    });
-});
-
-Goat
-    .then((data) => console.log("Of course: " + data))
-    .catch((error) => console.log("Nahhh: " + error));
+const cow = 0;
+while (cow < 20) {
+    console.log("Tujyende x nyine!");
+    cow++;
+}
